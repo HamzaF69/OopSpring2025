06669b78d988811f52a407710d80ab5b086fb069
HamzaF69 <136762872+HamzaF69@users.noreply.github.com>
2025-04-17 23:20:07 +0500

    Create task4.cpp

---
+++ lab9/task4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class PaymentMethod{
+    protected:
+    public:
+    virtual void processPayment(double amount)=0;
+};
+class CreditCard:public PaymentMethod{
+    private:
+    string cardNumber;
+    public:
+    CreditCard(string cn):cardNumber(cn){}
+    void processPayment(double amount)override{
+        cout<<"your amount "<< amount<<" is being processed\n";
+    }
+};
+class DigitalWallet:public PaymentMethod{
+    private:
+    double balance;
+    public:
+    DigitalWallet(double b):balance(b){}
+    void processPayment(double amount)override{
+        if(amount>balance){
+            cout<<"balance not enough\n";
+        }else{
+            cout<<"amount "<<amount<<" deducted from your account\nremain balance: "<<balance-amount;
+            balance-=amount;
+        }
+    }
+};
+
+int main(){
+    CreditCard c("1221HJK2112");
+    DigitalWallet d(500.00);
+    c.processPayment(50.00);
+    d.processPayment(100);
+}
