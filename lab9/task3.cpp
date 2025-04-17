04f67a159af70548e1cb6bdaae9673ae36ad2ed0
HamzaF69 <136762872+HamzaF69@users.noreply.github.com>
2025-04-17 23:17:59 +0500

    Create task3.cpp

---
+++ lab9/task3.cpp
@@ -0,0 +1,9 @@
+#include "Book.h"
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    Book h("harry potter", "jk rowling","AJ5544");
+    cout<<"title: "<<h.getTitle()<<"\nauthor: "<<h.getAuthor()<<"\nisbn"<<h.getISBN()<<endl;
+
+}
