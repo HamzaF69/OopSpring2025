2611de52e71258037719e8fc390907c08f859107
HamzaF69 <136762872+HamzaF69@users.noreply.github.com>
2025-05-04 16:47:44 +0500

    Create task2.cpp

---
+++ lab10/task2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+using namespace std;
+int main(){
+    ifstream ipFile("vehicles.txt");
+    string line;
+    if(ipFile.is_open()){
+        while(getline(ipFile,line)){
+            istringstream  s(line);
+            string type, id, name,year,data,cert;
+            getline(s,type,',');
+            getline(s,id,',');
+            getline(s,name,',');
+            getline(s,year,',');
+            getline(s,data,',');
+            getline(s,cert,',');
+            cout<<type<<endl;
+            cout<<id<<endl;
+            cout<<name<<endl;
+            cout<<year<<endl;
+            cout<<data<<endl;
+            cout<<cert<<endl;
+            cout<<endl;
+            
+        }
+    }else{
+        cout<<"couldnt open file\n";
+    }
+}
