16acc9341594d451df8118c2a874f86a8b46b686
HamzaF69 <136762872+HamzaF69@users.noreply.github.com>
2025-05-04 16:51:34 +0500

    Update task8.cpp

--- lab10/task8.cpp
+++ lab10/task8.cpp
@@ -3,15 +3,18 @@
 #include <sstream>
 #include <fstream>
 using namespace std;
+
 int main (){
-    ofstream oFile("sensor_log.txt");
+    ofstream ofile("config.txt");
+    ofile<<"AAAAABBBBBCCCCC";
+    ofile.close();
+    fstream oFile("config.txt",ios::in|ios::out);
+    oFile.seekp(5);
+    oFile<<"XXXXX";
+    oFile.close();
+
+    ifstream rfile("config.txt");
     string line;
-    oFile<<"Sensor 1: 25.5C\n";
-    streampos p1= oFile.tellp();
-    cout<<"current position: "<<p1<<endl;
-
-    oFile<<"Sensor 2: 98.1 %RH\n";
-    streampos p2= oFile.tellp();
-    cout<<"more recent position: "<<p2<<endl;
-
+    getline(rfile,line);
+    cout<<"updated content: "<<line;
 }
