3e3efec12cba85020027d1559125cce2d5bdf67d
HamzaF69 <136762872+HamzaF69@users.noreply.github.com>
2025-05-04 16:52:57 +0500

    Create task10.cpp

---
+++ lab10/task10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+using namespace std;
+int main (){
+    ifstream file("data_records.txt");
+    file.seekg(18,ios::beg);
+    string line;
+    getline(file,line);
+    cout<<"record: "<<line;
+}
