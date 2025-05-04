8ac8273efbcac3251a58033ff0ef0446c6fa3b0e
HamzaF69 <136762872+HamzaF69@users.noreply.github.com>
2025-05-04 20:15:21 +0500

    Update task10.cpp

--- lab10/task10.cpp
+++ lab10/task10.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main (){
     ifstream file("data_records.txt");
-    file.seekg(18,ios::beg);
+    file.seekg(20,ios::beg);
     string line;
     getline(file,line);
     cout<<"record: "<<line;
