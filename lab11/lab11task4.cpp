ed4eddc48813fb0695d91c3d19ba5ba0175eb979
HamzaF69 <136762872+HamzaF69@users.noreply.github.com>
2025-05-11 23:09:31 +0500

    Update lab11task4.cpp

--- lab11/lab11task4.cpp
+++ lab11/lab11task4.cpp
@@ -1,59 +1,52 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <fstream>
-#include <stdexcept>
+#include <exception>
 #include <cmath>
 #include <type_traits>
 using namespace std;
-
-class NegativeNumberException:public exception{
-    public:
+class NegativeNumberException : public exception {
+public:
     const char* what() const noexcept override {
         return "NegativeNumberException - number is less than zero\n";
     }
-
 };
-
-class InvalidTypeException:public exception{
-    public:
+class InvalidTypeException : public exception {
+public:
     const char* what() const noexcept override {
         return "InvalidTypeException - value is not a number\n";
     }
-
 };
-template<typename T>
-double sqrtVal(T num){
-    if(!is_arithmetic<T>::value){
-        throw InvalidTypeException();
-    }
 
-    if(num<0){
+template<typename T>
+typename enable_if<is_arithmetic<T>::value, double>::type
+sqrtVal(T num) {
+    if (num < 0) {
         throw NegativeNumberException();
     }
     return sqrt(static_cast<double>(num));
 }
+template<typename T>
+typename enable_if<!is_arithmetic<T>::value, double>::type
+sqrtVal(T) {
+    throw InvalidTypeException();
+}
 
-int main(){
-
+int main() {
     try {
         int m = 2;
         double result = sqrtVal(m);
         cout << "sqrt(" << m << ") = " << result << endl;
     }
-    catch (const InvalidTypeException& e) {
-        cout << "error: " << e.what() << endl;
-    }
-    catch (const NegativeNumberException& e) {
-        cout << "error: " << e.what() << endl;
+    catch (const exception& e) {
+        cout << "error: " << e.what();
     }
 
     try {
         string s = "hello";
-        double result = sqrtVal(s); // This will throw
-        cout << "sqrt(\"" << s << "\") = " << result << endl;
+        double result = sqrtVal(s);
+        cout << "sqrt(\"" << s<<"\") = " << result << endl;
     }
     catch (const exception& e) {
-        cout << "error: " << e.what() << endl;
+        cout << "error: " << e.what();
     }
 }
